9e2cfb0fc7f35b7706527573eb67195b6213306d
MangeshKhillare <mangeshkhillare10@gmail.com>
2023-02-20 09:03:09 +0530

    new class logic

--- Student.java
+++ Student.java
@@ -1,4 +1,8 @@
 public class Student{
+
+    public void mul(int a, int b){
+        System.out.println(a*b);
+    }
     public static void  main(String[] args){
         int a=10;
 int b=20;
@@ -7,5 +11,6 @@ int sum=a+b;
 System.out.println(sum);
 
     }
+
 
 }
